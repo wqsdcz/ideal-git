edbf0a272027db50722df547e76f541f78410f28
xcc <1272638358@qq.com>
2019-06-20 19:58:28 +0800

    增加删库跑路内容

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("我要删库跑路");
         System.out.println("我要删库跑路");
+        System.out.println("我要删库跑路");
     }
 }
